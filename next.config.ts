3b961f4f5f682d552ab45a7dfc91d01f61c98af8
OksanaMosk <oks-m3@ukr.net>
2025-09-03 01:45:05 +0300

    all

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import path from 'path';
+import type { NextConfig } from 'next';
+import type { Configuration } from 'webpack';
+
+const webpackConfig = (config: Configuration): Configuration => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+        ...(config.resolve.alias || {}),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@services': path.resolve(__dirname, 'src/services'),
+        '@models': path.resolve(__dirname, 'src/models'),
+        '@utils': path.resolve(__dirname, 'src/utils'),
+    };
+    return config;
+};
+
+const nextConfig: NextConfig = {
     images: {
         remotePatterns: [
             {
@@ -9,6 +24,7 @@ const nextConfig = {
             },
         ],
     },
+    webpack: webpackConfig,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
